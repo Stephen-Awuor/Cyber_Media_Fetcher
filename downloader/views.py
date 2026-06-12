ea865e640d245217bef8e709acb7bc8478b91fcd
Stephen-Awuor <stevekaneya@gmail.com>
2026-06-12 04:34:15 +0300

    Installed ytd package that analyses urls and sends back meatadata to the browser, Each format has a download option.

--- downloader/views.py
+++ downloader/views.py
@@ -1,17 +1,48 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import yt_dlp
 
 
 def home(request):
-
-    url = None
-
+    video = None
+    error = None
     if request.method == "POST":
         url = request.POST.get("url")
+        try:
+            ydl_opts = {
+                "quiet": True,
+                "noplaylist": True
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(
+                    url,
+                    download=False
+                )
+            video = {
+    "url": url,
+    "title": info.get("title"),
+    "thumbnail": info.get("thumbnail"),
+    "uploader": info.get("uploader"),
+    "duration": info.get("duration"),
+    "formats": info.get("formats", [])
+}
+        except Exception as e:
+            error = str(e)
 
     return render(
         request,
         "home.html",
         {
-            "url": url
+            "video": video,
+            "error": error
         }
+    )
+
+def download(request):
+
+    url = request.GET.get("url")
+    format_id = request.GET.get("format")
+
+    return HttpResponse(
+        f"Download requested: {format_id}"
     )
